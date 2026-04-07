4e4b7faa624c4b28b7d797c112c6967e8a051c1a
AlexxIacob <alexandruiacob01@gmail.com>
2026-04-07 04:10:34 +0300

    feat: added controller for user and tests for routes

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿namespace backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using backend.Services;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]   
+    public async Task<IActionResult> GetById(string id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        if (user is null)
+        {
+            return NotFound();
+        }
+        else
+            return Ok(user);
+    }
+ }
