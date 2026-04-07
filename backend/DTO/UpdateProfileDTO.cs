62435b9f18ead7083f260095f51eae05376a4fb9
AlexxIacob <alexandruiacob01@gmail.com>
2026-04-07 23:15:26 +0300

    feat: added ProfileUpdate DTO to pe used after register

---
+++ backend/DTO/UpdateProfileDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.DTO
+{
+    //in the given task for register there is needed only email and passwords 
+    //this dto is used after registration to update the users profile before he can actually do anything 
+    public class UpdateProfileDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+
+
+    }
+}
