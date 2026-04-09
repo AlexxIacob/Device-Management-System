5b6ed4409228af656054b84b17fd3460cdf92c82
AlexxIacob <alexandruiacob01@gmail.com>
2026-04-09 19:35:46 +0300

    feat:added login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { authGuard } from './guards/auth-guard';
 export const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', loadComponent: () => import('./pages/welcome/welcome').then(m => m.WelcomeComponent) },
+  { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent) },
 ];
--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class LoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    if (!this.email || !this.password) {
+      this.errorMessage = 'All fields are required.';
+      return;
+    }
+
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/devices']);
+      },
+      error: () => {
+        this.errorMessage = 'Invalid email or password.';
+      }
+    });
+  }
+}
